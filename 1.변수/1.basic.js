a1084640e1eb0a431deeb81b077d6be9bafe87e9
Soon9 <naong526@naver.com>
2021-01-05 11:58:47 +0900

    [21.01.05] # 변수, 식별자 학습완료

---
+++ 1.변수/1.basic.js
@@ -0,0 +1,22 @@
+
+
+//변수 선언
+var total;
+console.log(total);
+
+total = 100;
+console.log(total);
+
+//변수의 선언과 초기화를 동시에 진행
+var age = 27;
+console.log(age);
+
+total = 50;
+console.log(total);
+
+var copyAge = age;
+console.log(copyAge);
+
+var x = 10;
+x = x + 3;
+console.log(x);
---
+++ 1.변수/2.identifier.js
@@ -0,0 +1,33 @@
+
+var apple = "사과";
+var apple = "애플";
+console.log(apple);
+
+var banana = "바나나";
+var BANANA = "빠나나";
+console.log(banana);
+console.log(BANANA);
+
+// var my-name;
+var my_name; //(O)
+var $myname; //(O)
+
+// var 79num;
+var num79; //(O)
+var num79ber; //(O)
+
+// var var;
+// var if;
+// var my phone number;
+
+var myphonenumberforourcompany;
+var myPhoneNumberForOurCompany;
+
+//사용자 이름을 저장할거야 
+//구체적으로 지으세요 변수이름
+// var x;
+var userName;
+
+var p;
+
+var sagwa;
