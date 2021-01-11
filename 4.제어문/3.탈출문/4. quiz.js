f042d98de0b9b91bd85df3b9f43aca9d3957f85c
Soon9 <naong526@naver.com>
2021-01-11 09:41:28 +0900

    [21.01.11] # 제어문 퀴즈 정답

--- 4.제어문/3.탈출문/4. quiz.js
+++ 4.제어문/3.탈출문/4. quiz.js
@@ -12,6 +12,58 @@
    누적된 정답 횟수와 오답횟수를 출력하세요.
 */
 
+//정오답 횟수 카운트 변수
+var correctCount = 0, incorrectCount = 0;
+
+while (true) {
+   
+   //0 이상 101미만
+   var firstNumber = Math.floor(Math.random() * 101);
+   var secondNumber = Math.floor(Math.random() * 101);
+
+   
+
+   //랜덤 숫자 0,1 이 등장하도록 변수 선언.
+   //0: +, 1: - 
+   var operatorNumber = Math.floor(Math.random() * 2);
+
+   //랜덤 연산자 기호 만들기
+   // var opratorMark = (operatorNumber === 0) ? '+' : '-';
+   var opratorMark;
+   if (operatorNumber === 0) {
+      opratorMark = '+';
+   } else {
+      opratorMark = '-';
+   }
+
+   // 실제 정답
+   var total;
+   if (operatorMark === '+') {
+      total = firstNumber + secondNumber;
+   } else {
+      total = firstNumber - secondNumber;
+   }
+
+   // 일단 사용자가 입력한 답안
+   var userAnswser = +prompt(`${firstNumber} ${opratorMark} ${secondNumber} = ??`);
+   // alert(typeof userAnswser);
+   
+   if (userAnswser === 0) {
+      break;
+   }
+   
+   //정답 오답 판별
+   if (userAnswser === total) {
+      alert('정답입니다!');
+      correctCount++;
+   } else {
+      alert('오답입니다!');
+      incorrectCount++;
+   }
+}//반복문 종료
+
+alert(`프로그램이 종료되었습니다.
+\n# 정답 횟수: ${correctCount}회, 오답 횟수: ${incorrectCount}회`);
 
 
 
